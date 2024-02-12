5754c10919c8bd200ba1ba0904653b9f07f7906d
raymww <raymond.m.wang@gmail.com>
2024-02-12 13:30:38 -0600

    working api county call

---
+++ front-end/src/app/counties/page.jsx
@@ -0,0 +1,18 @@
+import Counties from "../components/counties"
+import { Row, Col, Container } from "react-bootstrap";
+import styles from "../page.module.css";
+
+export const metadata = {
+    title: "Counties"
+}
+
+export default function listCounties() {
+    return (
+        <main className= {styles.main}>
+            <div className= {styles.description}>
+                <h1>Counties Page</h1>
+            </div>
+            <Counties/>
+        </main>
+    )
+}
