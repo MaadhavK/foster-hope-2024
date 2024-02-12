ffce6defbe9a511427a973f8f87a42a6b1736022
gracep <gracepan@wireless-10-155-245-142.public.utexas.edu>
2024-02-12 17:28:50 -0600

    axios???

--- front-end/src/app/about/components/aboutCard.js
+++ front-end/src/app/about/components/aboutCard.js
@@ -1,4 +1,4 @@
-
+"use client"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
@@ -6,29 +6,43 @@ const id = "54579149x"
 const token = "glpat-UCq8eByXaqn8Jzxw96g1"
 const headers = {
   'Authorization': `BEARER ${token}`
-};
-
-async function getMemberCommit({username}) {
-  const res = await fetch(`https://gitlab.com/projects/${id}/repository/commits?author_username=${username}`,{headers} )
-  if (!res.ok) {
-    throw new Error("No data")
-  }
-  return await res.json()
 }
+// };
 
-async function getMemberIssue({username}) {
-  const res = await fetch(`https://gitlab.com/projects/${id}/issue_statistics?assignee_id=${username}`, {headers})
-  if (!res.ok) {
-    throw new Error("No data")
-  }
-  return await res.json()
-}
+// async function getMemberCommit({username}) {
+//   const res = await fetch(`https://gitlab.com/projects/${id}/repository/commits?author_username=${username}`,{headers} )
+//   if (!res.ok) {
+//     throw new Error("No data")
+//   }
+//   return await res.json()
+// }
 
-export default async function AboutCard({member}) {
-  const commit = await getMemberCommit(member.username)
-  const issue = await getMemberIssue(member.username)
+// async function getMemberIssue({username}) {
+//   const res = await fetch(`https://gitlab.com/projects/${id}/issue_statistics?assignee_id=${username}`, {headers})
+//   if (!res.ok) {
+//     throw new Error("No data")
+//   }
+//   return await res.json()
+// }
+
+export default function AboutCard({member}) {
   console.log(commit)
   console.log(issue)
+    const [commit, setCommit] = useState(null);
+    const [issue, setIssue] = useState(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('https://gitlab.com/projects/${id}/repository/commits?author_username=${username}', headers);
+          setCommit(response.data);
+        } catch (error) {
+          setError(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
   return (
     <Card style = {{width: "20rem"}}>
        <Card.Img variant="top" src={"http://localhost:3001" + member.img}/>
