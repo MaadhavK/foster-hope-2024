34db76df4dc2fe3e2bc27ba551225817fb0132a5
Nathan Cheng <nathanchengus@gmail.com>
2024-02-13 01:38:31 -0600

    Removed nav bar

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -9,11 +9,10 @@ export const metadata = {
   title: "Foster Hope",
   description: "An informational website serving those in foster care in Texas and those wanting to contribute",
 };
-
+//    <NavBar/>
 export default function RootLayout({ children }) {
   return (
     <>
-    <NavBar/>
     <main className= {inter.className}>{children}</main>
     </>
   );
