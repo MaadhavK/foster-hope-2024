66cd3a7e3fe3a5a94e1f5ced59b1bcad8d61f103
AnanthKothuri <maadhavskothuri@gmail.com>
2024-02-13 13:40:36 -0600

    Made layout.js client side rendered

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,14 +1,10 @@
+"use client"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Foster Hope",
-  description: "An informational website serving those in foster care in Texas and those wanting to contribute",
-};
 
 export default function RootLayout({ children }) {
   return (
