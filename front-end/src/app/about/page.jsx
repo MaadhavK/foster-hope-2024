7116942a9c98bcfd6dc9e570a489d70c3fbf201e
AnanthKothuri <maadhavskothuri@gmail.com>
2024-02-13 19:52:58 -0600

    Added back other pages

--- front-end/src/app/about/page.jsx
+++ front-end/src/app/about/page.jsx
@@ -52,7 +52,7 @@ export default function About() {
                 <h1>About Us</h1>
                 </div>
 
-            {/* <Container style = {{padding: 15}}>
+            <Container style = {{padding: 15}}>
                 <Row style = {{padding: "2rem"}}>
                     {data["members"].slice(0, 3).map((member) => (
                         <Col xs> <AboutCard member={member}/> </Col>
@@ -64,7 +64,7 @@ export default function About() {
                     ))}
                 </Row>
                 
-            </Container> */}
+            </Container>
         </main>
         </>
     )
