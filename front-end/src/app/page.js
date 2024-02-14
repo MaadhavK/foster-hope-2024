2cf1637f41140d2d12d91a8c1f6923917e072936
Nathan Cheng <nathanchengus@gmail.com>
2024-02-14 00:40:55 -0600

    Added splash page

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -8,11 +8,17 @@ import _default from "react-bootstrap/esm/Nav";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <div className={styles.description}>
-          <h1>Foster Hope</h1>
-          <h2>Description</h2>
-        </div>
+
+    <main className={styles.main2}>
+
+        <h1 className={styles.topspace}>What We Do</h1>
+        <p className={styles.splashdesc}>
+          This website is intended to be a source of information and resources about foster care in Texas.
+          The intended audience are those curious about foster care and those in foster care needing resources.
+        </p>
+        <img class={styles.splashimage} src="/images/FrontPageImage.jpg" />
+
+
       </main>
   );
 }
