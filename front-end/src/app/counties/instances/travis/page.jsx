ff6eb6e326ab2d63228b89fb2e49af4468070aa4
AnanthKothuri <maadhavskothuri@gmail.com>
2024-02-14 00:29:25 -0600

    Added instance pages for counties and buttons to link to them

---
+++ front-end/src/app/counties/instances/travis/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import styles from "../../../page.module.css";
+
+export default function TravisCounty(){
+    return (
+        <main className = {styles.main}>
+            <div className={styles.description}>
+                <h1> Travis County Information</h1>
+                <p> Insert information, multimedia, and cards for other models</p>
+            </div>
+        </main>
+    )
+}
--- front-end/src/app/counties/page.jsx
+++ front-end/src/app/counties/page.jsx
@@ -1,14 +1,16 @@
 "use client"
 // import Counties from "./components/countyCardList"
-import { Row, Col, Container, Card} from "react-bootstrap";
+import { Row, Col, Container, Card, Button} from "react-bootstrap";
 import styles from "../page.module.css";
+
+import Link from "next/link";
 
 // export const metadata = {
 //     title: "Counties"
 // }
 
 export default function listCounties() {
-
+    
     //const Counties = dynamic(() => import("./components/countyCardList"));
     return (
         <main className= {styles.main}>
@@ -31,7 +33,9 @@ export default function listCounties() {
                                 Number of Foster Children: 186
                                 <br></br>
                                 Number of Foster Homes: 20
+                                <br></br>
                                 </Card.Text>
+                                <Button href = "counties/instances/travis/"> Read More </Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -49,6 +53,7 @@ export default function listCounties() {
                                 <br></br>
                                 Number of Foster Homes: 20
                                 </Card.Text>
+                                <Button href = "counties/instances/hays/"> Read More </Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -66,6 +71,7 @@ export default function listCounties() {
                                 <br></br>
                                 Number of Foster Homes: 20
                                 </Card.Text>
+                                <Button href = "counties/instances/bastrop/"> Read More </Button>
                             </Card.Body>
                         </Card>
                     </Col>
