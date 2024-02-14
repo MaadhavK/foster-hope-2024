ee31a1961c40a58b550b9ffd0dfa5f107dbddc5c
AnanthKothuri <maadhavskothuri@gmail.com>
2024-02-14 16:43:28 -0600

    Made Navbar responsive

--- front-end/src/app/components/NavBar.js
+++ front-end/src/app/components/NavBar.js
@@ -9,15 +9,18 @@ import styles from "../page.module.css"
 export default function NavBar(){
     return (
         <>
-            <Navbar bg="light" data-bs-theme="light" fixed = "top" border="white" >
-            <Container className={styles.container}>
-            <Navbar.Brand href="/">Foster Hope</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="/about">About Us</Nav.Link>
-                <Nav.Link href="/counties">Counties</Nav.Link>
-                <Nav.Link href="/organizations">Organizations</Nav.Link>
-                <Nav.Link href="/resources">Resources</Nav.Link>
-            </Nav>
+            <Navbar bg="light" data-bs-theme="light" fixed = "top" border="white" expand="lg" collapseOnSelect>
+                <Container className={styles.container}>
+                <Navbar.Brand href="/">Foster Hope</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/about">About Us</Nav.Link>
+                    <Nav.Link href="/counties">Counties</Nav.Link>
+                    <Nav.Link href="/organizations">Organizations</Nav.Link>
+                    <Nav.Link href="/resources">Resources</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
         <br />
