6721678068465ace30df2c64b71c065c6224dcd9
AnanthKothuri <maadhavskothuri@gmail.com>
2024-02-14 19:13:08 -0600

    Styled the splash page a bit

--- front-end/src/app/components/carousel.js
+++ front-end/src/app/components/carousel.js
@@ -9,12 +9,8 @@ function HomeCarousel() {
   ];
   const CarouselItems = (images) => {
     return images.map((img, idx) => (
-          <Carousel.Item key = {idx} interval={1000} style={{width:"100%", height:"30rem", background: "rgba(0,0,0.5)"}}>
+          <Carousel.Item key = {idx} interval={3000} style={{width:"100%", height:"30rem", background: "rgba(0,0,0.5)"}}>
             <Image src={img.src} fluid style={{objectFit: "cover", width:"100%", height:"100%", opacity: "0.8", borderRadius: "10px"}}/>
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           
       ));
