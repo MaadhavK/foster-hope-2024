9c0eeb3af27d8a20e772379eeab642c205f795a3
raymww <raymond.m.wang@gmail.com>
2024-02-23 03:59:48 -0600

    splash page bruh

--- front-end/src/app/components/NavBar.js
+++ front-end/src/app/components/NavBar.js
@@ -9,21 +9,20 @@ import styles from "../page.module.css"
 export default function NavBar(){
     return (
         <>
-            <Navbar bg="dark" data-bs-theme="dark" fixed = "top" border="white" expand="lg">
-                <Container className={styles.container}>
-                <Navbar.Brand href="/">Foster Hope</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/about">About Us</Nav.Link>
-                    <Nav.Link href="/counties">Counties</Nav.Link>
-                    <Nav.Link href="/organizations">Organizations</Nav.Link>
-                    <Nav.Link href="/resources">Resources</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        <br />
+            <Navbar bg="light" data-bs-theme="light" fixed = "top" border="white" expand="lg">
+                <Container className={styles.container} style={{maxWidth:"100vw", paddingLeft:"5vw"}}>
+                    <Navbar.Brand href="/">Foster Hope</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/about">About Us</Nav.Link>
+                        <Nav.Link href="/counties">Counties</Nav.Link>
+                        <Nav.Link href="/organizations">Organizations</Nav.Link>
+                        <Nav.Link href="/resources">Resources</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </>
     )
     
