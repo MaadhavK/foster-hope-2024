731f79acb86b56079b65dfd1fa8f3a8d7d325e42
Nathan Cheng <nathanchengus@gmail.com>
2024-03-05 22:32:33 -0600

    Stuff

--- back-end/scrapping/media.py
+++ back-end/scrapping/media.py
@@ -1,8 +1,8 @@
 import os
-from supabase import create_client, Client
 import requests
 import json
 from dotenv import load_dotenv
+from supabase import create_client
 
 
 # Load environment variables from .env file
@@ -10,20 +10,30 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
 google_key = os.environ.get("GOOGLE_API_KEY")
 engine_id = os.environ.get("ENGINE_ID")
 img_engine_id = os.environ.get("IMG_SEARCH_ENGINE_ID")
 
+# Grab .env keys
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+
 supabase = create_client(url, key)
+
 
 def store_county_videos():
     #get the name of counties we need to find videos for
-    response = supabase.table('FosterHomesPerCounty').select('county').execute()
+    #response = supabase.table('Counties').select('county').eq('media', None).execute()
+    
+    response = supabase.table('Counties').select('county').execute()
     counties= set(row['county'] for row in response.data)
     print(counties)
     for county in counties:
+        
+        youtube_link = supabase.table('Counties').select('media').eq('county', county).execute()
+        if youtube_link.data[0].get('media') != None:
+            print(county+" already has videos")
+            continue
         api_endpoint = 'https://www.googleapis.com/youtube/v3/search'
         params = {
             'key': google_key,
@@ -41,14 +51,15 @@ def store_county_videos():
                 continue
             video_id = data['items'][0]['id']['videoId']
             media_link = 'https://www.youtube.com/watch?v=' + video_id
-            supabase.table('FosterHomesPerCounty').update({'media': media_link}).eq('county', county).execute()
+            supabase.table('Counties').update({'media': media_link}).eq('county', county).execute()
             print('sucessful update')
         else:
             print('unsuccesssful update')
             print(county)
 
 def store_county_images():
-    response = supabase.table('FosterHomesPerCounty').select('county').execute()
+    #response = supabase.table('Counties').select('county').execute()
+    response = supabase.table('Counties').select('county').eq('media', None).execute()
     counties= list(row['county'] for row in response.data)
     for county in reversed(counties):
         api_endpoint = 'https://www.googleapis.com/customsearch/v1'
@@ -67,12 +78,12 @@ def store_county_images():
             if not data:
                 continue
             img_link = data['items'][0]['link']
-            supabase.table('FosterHomesPerCounty').update({'image': img_link}).eq('county', county).execute()
+            supabase.table('Counties').update({'image': img_link}).eq('county', county).execute()
             print('sucessful update')
         else:
             print('unsuccesssful update')
             print(county)
-#store_county_videos()
-store_county_images()
+store_county_videos()
+#store_county_images()
 
 
