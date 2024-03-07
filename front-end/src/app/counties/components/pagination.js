88215e88ef0b23a9313400f22af0eba3a46f6906
AnanthKothuri <maadhavskothuri@gmail.com>
2024-03-06 21:15:34 -0600

    Counties Pagination working

--- front-end/src/app/counties/components/pagination.js
+++ front-end/src/app/counties/components/pagination.js
@@ -1,7 +1,7 @@
 "use client"
 import { useRouter, useSearchParams} from 'next/navigation';
-import React, {useState} from 'react';
-
+import React, {useState, useEffect} from 'react';
+import "../counties.css";
 
 const Pagination = ({num_instances}) => {
 
@@ -12,34 +12,53 @@ const Pagination = ({num_instances}) => {
     const page = searchParams.get('page') ?? '1'
     const per_page = searchParams.get('per_page') ?? '16'
 
-    
-    const [hasNextPage, setHasNextPage] = useState(true);
-    const [hasPrevPage, setHasPrevPage] = useState((hasPrevPage) => page > 1 ? true : false);
+    const [pageState, setPageState] = useState( {
+        prevPage : 0,
+        nextPage : 2,
+        hasPrevPage: false,
+        hasNextPage: true
+    });
 
     return (
     <div className='flex gap-2'>
       <button
         className='bg-blue-500 text-white p-1'
-        disabled={!hasPrevPage}
+        disabled={!pageState.hasPrevPage}
         onClick={() => {
           router.push(`/counties/?page=${Number(page) - 1}&per_page=${per_page}`)
-          setHasPrevPage(page> 1)
-        }}>
+            setPageState({
+                    prevPage: pageState.prevPage - 1,
+                    nextPage: pageState.nextPage - 1,
+                    hasPrevPage: pageState.prevPage - 1> 0,
+                    hasNextPage: true
+                });
+          
+        }} 
+        >
         prev page
       </button>
 
-      <div>
-        {page} / {Math.ceil(10 / Number(per_page))}
+      <div className='text-black'>
+        {pageState.prevPage + 1} / {Math.ceil(Number(num_instances)/Number(per_page))}
+        
       </div>
 
       <button
         className='bg-blue-500 text-white p-1'
-        disabled={!hasNextPage}
+        disabled={!pageState.hasNextPage}
         onClick={() => {
           router.push(`/counties/?page=${Number(page) + 1}&per_page=${per_page}`)
-          setHasNextPage(page < Math.ceil(num_instances / per_page))
+            setPageState({
+                prevPage: pageState.prevPage + 1,
+                nextPage: pageState.nextPage + 1,
+                hasPrevPage: true,
+                hasNextPage: pageState.nextPage + 1 < Math.ceil(Number(num_instances)/Number(per_page))
+            });
+            
+          
         }}>
         next page
+        
       </button>
     </div>
   )
