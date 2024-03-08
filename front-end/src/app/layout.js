a652adf9e47566189b37bd00be91991aea61fa42
sakura-lxver <aleanadhiraa@gmail.com>
2024-03-08 13:39:22 -0600

    added logo Closes #91

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -3,12 +3,16 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang = "en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     <body>
       <NavBar/>
       <main className={inter.className}>{children}</main>
