213eed3515c5a91d45a4bb9bb44a1516bde74fa8
sakura-lxver <aleanadhiraa@gmail.com>
2024-04-03 19:25:05 -0500

    added searching and sorting

--- back-end/app/routes.py
+++ back-end/app/routes.py
@@ -31,7 +31,7 @@ def all_counties_search():
         query = ""
 
         if search_query:
-            query = text('SELECT * FROM "Counties" WHERE county ILIKE :search_query OR number_of_homes ILIKE :search_query OR number_of_foster_kids ILIKE :search_query OR population ILIKE :search_query OR description ILIKE :search_query OR number_of_orgs = :int_search_query')
+            query = text('SELECT * FROM "Counties" WHERE county ILIKE :search_query OR number_of_homes ILIKE :search_query OR number_of_foster_kids ILIKE :search_query OR population ILIKE :search_query OR description ILIKE :search_query OR number_of_orgs = :search_query')
         else:
             return None
         # Execute the query with parameters based on their types
@@ -59,12 +59,31 @@ def all_counties_sort():
         data = [x._asdict() for x in result.all()]
         return json.JSONEncoder().encode({"data": data})  
 
+# @app.route('/counties/all_counties')
+# def all_counties():
+#     with engine.connect() as connection:
+#         query = text('SELECT * FROM "Counties"')
+        
+#         result = connection.execute(query)
+#         data = [x._asdict() for x in result.all()]
+#         return json.JSONEncoder().encode({"data": data})
 @app.route('/counties/all_counties')
 def all_counties():
+    search_query = request.args.get('search_query')
+    sort = request.args.get('sort')
+    int_search_query = None
     with engine.connect() as connection:
-        query = text('SELECT * FROM "Counties"')
+        query = 'SELECT * FROM "Counties"'
+        if search_query:
+            query += ' WHERE county ILIKE :search_query OR number_of_homes ILIKE :search_query OR number_of_foster_kids ILIKE :search_query OR population ILIKE :search_query OR description ILIKE :search_query OR number_of_orgs = :int_search_query'
+            if search_query.isdigit():
+                int_search_query = int(search_query)
+        if sort:
+            descending = sort[0] == '-' 
+            field = sort[1:] if descending else sort
+            query += ' ORDER BY "{}" {}'.format(field, "DESC" if descending else "ASC")
         
-        result = connection.execute(query)
+        result = connection.execute(text(query), {'search_query': f'%{search_query}%', 'int_search_query': int_search_query})
         data = [x._asdict() for x in result.all()]
         return json.JSONEncoder().encode({"data": data})
 
@@ -82,49 +101,69 @@ def single_org():
         data = [x._asdict() for x in result.all()]
         return json.JSONEncoder().encode({"data": data})
 
+# @app.route('/orgs/all_orgs')
+# def all_orgs():
+#     with engine.connect() as connection:
+#         query = text('SELECT * FROM "Organizations"')
+        
+#         result = connection.execute(query)
+#         data = [x._asdict() for x in result.all()]
+#         return json.JSONEncoder().encode({"data": data})
+    
 @app.route('/orgs/all_orgs')
 def all_orgs():
+    search_query = request.args.get('search_query')
+    sort = request.args.get('sort')
+    int_search_query = 0
     with engine.connect() as connection:
-        query = text('SELECT * FROM "Organizations"')
-
-        result = connection.execute(query)
+        query = 'SELECT * FROM "Organizations"'
+        if search_query:
+            query += ' WHERE name ILIKE :search_query OR location ILIKE :search_query OR operation_hours ILIKE :search_query OR type ILIKE :search_query OR description ILIKE :search_query OR rating = :int_search_query'
+            if search_query.isdigit():
+                int_search_query = int(search_query)
+        if sort:
+            descending = sort[0] == '-' 
+            field = sort[1:] if descending else sort
+            query += ' ORDER BY "{}" {}'.format(field, "DESC" if descending else "ASC")
+        
+        result = connection.execute(text(query), {'search_query': f'%{search_query}%', 'int_search_query': int_search_query})
         data = [x._asdict() for x in result.all()]
         return json.JSONEncoder().encode({"data": data})
     
-@app.route('/orgs/all_orgs_search')
-def all_orgs_search():
-    with engine.connect() as connection:
-        search_query = request.args.get("search_query")
-        int_search_query = 0
-        query = ""
+# @app.route('/orgs/all_orgs_search')
+# def all_orgs_search():
+#     with engine.connect() as connection:
+#         search_query = request.args.get("search_query")
+#         int_search_query = 0
+#         query = ""
 
-        if search_query:
-            query = text('SELECT * FROM "Organizations" WHERE name ILIKE :search_query OR location ILIKE :search_query OR operation_hours ILIKE :search_query OR type ILIKE :search_query OR description ILIKE :search_query OR rating = :int_search_query')
-        else:
-            return None
-        if search_query.isdigit(): 
-            int_search_query = int(search_query)
-            result = connection.execute(query, {'search_query': f'%{search_query}%', 'int_search_query': int_search_query})
-        else:
-            result = connection.execute(query, {'search_query': f'%{search_query}%', 'int_search_query': 0})  # Provide a default value for int_val if val is not a digit
+#         if search_query:
+#             query = text('SELECT * FROM "Organizations" WHERE name ILIKE :search_query OR location ILIKE :search_query OR operation_hours ILIKE :search_query OR type ILIKE :search_query OR description ILIKE :search_query OR rating = :search_query')
+#         else:
+#             return None
+#         if search_query.isdigit(): 
+#             int_search_query = int(search_query)
+#             result = connection.execute(query, {'search_query': f'%{search_query}%', 'int_search_query': int_search_query})
+#         else:
+#             result = connection.execute(query, {'search_query': f'%{search_query}%', 'int_search_query': 0})  # Provide a default value for int_val if val is not a digit
 
-        data = [x._asdict() for x in result.all()]
-        return json.JSONEncoder().encode({"data": data})    
+#         data = [x._asdict() for x in result.all()]
+#         return json.JSONEncoder().encode({"data": data})    
 
-@app.route('/orgs/all_orgs_sort')
-def all_orgs_sort():
-    sorts = request.args.get('sort')
-    with engine.connect() as connection:
-        query = 'SELECT * FROM "Organizations"'
-        if sorts:
-            for sort in sorts.split(","):
-                descending = sort[0] == '-' 
-                field = sort[1:] if descending else sort
-                query += f' ORDER BY "{field}" {"DESC" if descending else "ASC"},'
-            query = query.rstrip(',')
-        result = connection.execute(text(query))
-        data = [x._asdict() for x in result.all()]
-        return json.JSONEncoder().encode({"data": data})
+# @app.route('/orgs/all_orgs_sort')
+# def all_orgs_sort():
+#     sorts = request.args.get('sort')
+#     with engine.connect() as connection:
+#         query = 'SELECT * FROM "Organizations"'
+#         if sorts:
+#             for sort in sorts.split(","):
+#                 descending = sort[0] == '-' 
+#                 field = sort[1:] if descending else sort
+#                 query += f' ORDER BY "{field}" {"DESC" if descending else "ASC"},'
+#             query = query.rstrip(',')
+#         result = connection.execute(text(query))
+#         data = [x._asdict() for x in result.all()]
+#         return json.JSONEncoder().encode({"data": data})
     
 @app.route('/resources/single_resource')
 def resources():
@@ -152,39 +191,57 @@ def all_resource_type():
         data = [x._asdict() for x in result.all()]
         return json.JSONEncoder().encode({"data": data})
     
+# @app.route('/resources/all_resources')
+# def all_resources():
+#     with engine.connect() as connection:
+#         query = query = text('SELECT * FROM "Resources"')
+
+#         result = connection.execute(query)
+#         data = [x._asdict() for x in result.all()]
+#         return json.JSONEncoder().encode({"data": data})
+
 @app.route('/resources/all_resources')
 def all_resources():
+    search_query = request.args.get('search_query')
+    sort = request.args.get('sort')
     with engine.connect() as connection:
-        query = query = text('SELECT * FROM "Resources"')
-
-        result = connection.execute(query)
+        query = 'SELECT * FROM "Resources"'
+        if search_query:
+            query += ' WHERE name ILIKE :search_query OR location ILIKE :search_query OR hours ILIKE :search_query OR type ILIKE :search_query OR description ILIKE :search_query'
+        if sort:
+            descending = sort[0] == '-' 
+            field = sort[1:] if descending else sort
+            query += ' ORDER BY "{}" {}'.format(field, "DESC" if descending else "ASC")
+        
+        result = connection.execute(text(query), {'search_query': f'%{search_query}%'})
         data = [x._asdict() for x in result.all()]
         return json.JSONEncoder().encode({"data": data})
     
-@app.route('/resources/all_resources_search')
-def all_resources_search():
-    search_query = request.args.get('search_query') # get search query from request params
-    with engine.connect() as connection:
-        if search_query:
-            query = text('SELECT * FROM "Resources" WHERE name ILIKE :search_query OR location ILIKE :search_query OR hours ILIKE :search_query OR type ILIKE :search_query OR description ILIKE :search_query')
-            result = connection.execute(query, {'search_query': f'%{search_query}%'})
-        else:
-            query = text('SELECT * FROM "Resources"')
-            result = connection.execute(query)
-        data = [x._asdict() for x in result.all()]
-        return json.JSONEncoder().encode({"data": data})
+# @app.route('/resources/all_resources_search')
+# def all_resources_search():
+#     search_query = request.args.get('search_query') # get search query from request params
+#     with engine.connect() as connection:
+#         if search_query:
+#             query = text('SELECT * FROM "Resources" WHERE name ILIKE :search_query OR location ILIKE :search_query OR hours ILIKE :search_query OR type ILIKE :search_query OR description ILIKE :search_query')
+#             result = connection.execute(query, {'search_query': f'%{search_query}%'})
+#         else:
+#             query = text('SELECT * FROM "Resources"')
+#             result = connection.execute(query)
+#         data = [x._asdict() for x in result.all()]
+#         return json.JSONEncoder().encode({"data": data})
 
-@app.route('/resources/all_resources_sort')
-def all_resources_sort():
-    sorts = request.args.get('sort')
-    with engine.connect() as connection:
-        query = 'SELECT * FROM "Resources"'
-        if sorts:
-            for sort in sorts.split(","):
-                descending = sort[0] == '-' 
-                field = sort[1:] if descending else sort
-                query += f' ORDER BY "{field}" {"DESC" if descending else "ASC"},'
-            query = query.rstrip(',')
-        result = connection.execute(text(query))
-        data = [x._asdict() for x in result.all()]
-        return json.JSONEncoder().encode({"data": data})
+# @app.route('/resources/all_resources_sort')
+# def all_resources_sort():
+#     sorts = request.args.get('sort')
+#     with engine.connect() as connection:
+#         query = 'SELECT * FROM "Resources"'
+#         if sorts:
+#             for sort in sorts.split(","):
+#                 descending = sort[0] == '-' 
+#                 field = sort[1:] if descending else sort
+#                 query += f' ORDER BY "{field}" {"DESC" if descending else "ASC"},'
+#             query = query.rstrip(',')
+#         result = connection.execute(text(query))
+#         data = [x._asdict() for x in result.all()]
+#         return json.JSONEncoder().encode({"data": data})
+  
